c60b21f78ec0d8484d8cf67293896e854dc9d2f7
caohuilin <caohuilin@bytedance.com>
2021-11-05 11:35:22 +0800

    feat: test get package manager

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,26 @@
+import path from 'path';
+import os from 'os';
 import * as core from '@actions/core';
 import fs from 'fs-extra';
-import { gitConfigUser, gitCommitAll, gitPushTags } from './git';
-import {
-  bumpCanaryVersion,
-  changeDependenceVersion,
-  runInstall,
-  runPrepare,
-  runRelease,
-  writeNpmrc,
-} from './utils';
 
-// eslint-disable-next-line max-statements
 (async () => {
-  const githubToken = process.env.GITHUB_TOKEN;
-  const publishVersion = core.getInput('version');
-  console.info('publishVersion', publishVersion);
-
-  if (!githubToken) {
-    core.setFailed('Please add the GITHUB_TOKEN');
-    return;
+  const cwd = process.cwd();
+  let appDirectory = cwd;
+  while (os.homedir() !== appDirectory) {
+    console.info('homeDir', os.homedir());
+    console.info('appDirectory', appDirectory);
+    if (fs.existsSync(path.resolve(appDirectory, 'pnpm-lock.yaml'))) {
+      return 'pnpm';
+    }
+    if (fs.existsSync(path.resolve(appDirectory, 'yarn.lock'))) {
+      return 'yarn';
+    }
+    if (fs.existsSync(path.resolve(appDirectory, 'package-lock.json'))) {
+      return 'npm';
+    }
+    appDirectory = path.join(appDirectory, '..');
   }
-
-  await gitConfigUser();
-
-  await fs.writeFile(
-    `${process.env.HOME as string}/.netrc`,
-    `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`,
-  );
-
-  // hack modern.js repo need to change plugin-testing and module-tools version
-  const repo = process.env.REPOSITORY;
-
-  if (repo === 'modern-js-dev/modern.js' && publishVersion !== 'canary') {
-    await changeDependenceVersion();
-  }
-
-  // prepare repo
-  await runInstall();
-  await runPrepare();
-
-  await writeNpmrc();
-  // publish
-  if (publishVersion === 'canary') {
-    await bumpCanaryVersion(publishVersion);
-    await gitCommitAll('publish canary');
-    // await runRelease(process.cwd(), 'canary');
-  } else if (publishVersion === 'pre') {
-    await gitCommitAll('publish pre');
-    await runRelease(process.cwd(), 'next');
-    // push tags
-    await gitPushTags();
-  } else {
-    await gitCommitAll('publish latest');
-    await runRelease();
-    // push tags
-    await gitPushTags();
-  }
+  return 'npm';
 })().catch(err => {
   console.error(err);
   core.setFailed(err.message);
